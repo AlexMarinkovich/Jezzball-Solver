2c00d2f6139da6377337368aacab1073417b3aa6
xinfay <21sg142@queensu.ca>
2023-10-30 16:29:20 -0400

    Update run.py

--- run.py
+++ run.py
@@ -9,77 +9,117 @@ config.sat_backend = "kissat"
 # Encoding that will store all of your constraints
 E = Encoding()
 
-# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-@proposition(E)
-class BasicPropositions:
+class Cursor():
+    def __init__(self,x,y,is_hori,is_vert):
+        self.x = x
+        self.y = y
+        #
+        #   may adjust so self.is_vert = not is_hori
+        #   this means that only one of these two variables will exist
+        #   based on own constraints: (H | V) & ~(H & V)
+        #
+        self.is_hori = is_hori
+        self.is_vert = is_vert
 
-    def __init__(self, data):
-        self.data = data
+    def __str__(self) -> str:
+        return f"Cursor located at x:{self.x} y:{self.y},\n
+        Horizontal:{self.is_hori} Vertical:{self.is_vert}"
+    
+class Ball():
+    def __init__(self,x,y,pos_hori_dir,pos_vert_dir):
+        self.x = x
+        self.y = y
+        self.pos_hori_dir = pos_hori_dir
+        self.pos_vert_dir = pos_vert_dir
 
-    def __repr__(self):
-        return f"A.{self.data}"
+    def __str__(self) -> str:
+        return f"Ball located at x:{self.x} y:{self.y},\n
+        Horizontal direction:{self.is_hori} Vertical direction:{self.is_vert}"
+    
+class Builder():
+    def __init__(self,x,y,is_hori,is_vert):
+        self.x = x
+        self.y = y
+        self.is_hori = is_hori
+        self.is_vert = is_vert
 
-
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
-@proposition(E)
-class FancyPropositions:
-
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        return f"A.{self.data}"
-
-# Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")   
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
-# At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
-
-
-# Build an example full theory for your setting and return it.
-#
-#  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
-#  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
-#  what the expectations are.
-def example_theory():
-    # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
-    # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint(~(x & y))
-    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
-
-    return E
+    def __str__(self) -> str:
+        return f"Builder located at x:{self.x} y:{self.y},\n
+        Horizontal build:{self.is_hori} Vertical build:{self.is_vert}"
 
 
-if __name__ == "__main__":
+# USE ONLY AS REFERENCE
+# # To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
+# @proposition(E)
+# class BasicPropositions:
 
-    T = example_theory()
-    # Don't compile until you're finished adding all your constraints!
-    T = T.compile()
-    # After compilation (and only after), you can check some of the properties
-    # of your model:
-    print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
-    print("   Solution: %s" % T.solve())
+#     def __init__(self, data):
+#         self.data = data
 
-    print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-        # Ensure that you only send these functions NNF formulas
-        # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
-    print()
+#     def __repr__(self):
+#         return f"A.{self.data}"
+
+
+# # Different classes for propositions are useful because this allows for more dynamic constraint creation
+# # for propositions within that class. For example, you can enforce that "at least one" of the propositions
+# # that are instances of this class must be true by using a @constraint decorator.
+# # other options include: at most one, exactly one, at most k, and implies all.
+# # For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
+# @constraint.at_least_one(E)
+# @proposition(E)
+# class FancyPropositions:
+
+#     def __init__(self, data):
+#         self.data = data
+
+#     def __repr__(self):
+#         return f"A.{self.data}"
+
+# # Call your variables whatever you want
+# a = BasicPropositions("a")
+# b = BasicPropositions("b")   
+# c = BasicPropositions("c")
+# d = BasicPropositions("d")
+# e = BasicPropositions("e")
+# # At least one of these will be true
+# x = FancyPropositions("x")
+# y = FancyPropositions("y")
+# z = FancyPropositions("z")
+
+
+# # Build an example full theory for your setting and return it.
+# #
+# #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
+# #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
+# #  what the expectations are.
+# def example_theory():
+#     # Add custom constraints by creating formulas with the variables you created. 
+#     E.add_constraint((a | b) & ~x)
+#     # Implication
+#     E.add_constraint(y >> z)
+#     # Negate a formula
+#     E.add_constraint(~(x & y))
+#     # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
+#     # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
+#     constraint.add_exactly_one(E, a, b, c)
+
+#     return E
+
+
+# if __name__ == "__main__":
+
+#     T = example_theory()
+#     # Don't compile until you're finished adding all your constraints!
+#     T = T.compile()
+#     # After compilation (and only after), you can check some of the properties
+#     # of your model:
+#     print("\nSatisfiable: %s" % T.satisfiable())
+#     print("# Solutions: %d" % count_solutions(T))
+#     print("   Solution: %s" % T.solve())
+
+#     print("\nVariable likelihoods:")
+#     for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
+#         # Ensure that you only send these functions NNF formulas
+#         # Literals are compiled to NNF here
+#         print(" %s: %.2f" % (vn, likelihood(T, v)))
+#     print()
