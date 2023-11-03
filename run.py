3554bf46b8989bf4f72c754027e32241aca373fd
Alexander G Marinkovich <alex-989@live.com>
2023-11-02 23:13:19 -0400

    final change before submitting draft

--- run.py
+++ run.py
@@ -1,140 +1,208 @@
+CANV_CELLS_WIDTH = 27
+CANV_CELLS_HEIGHT = 19
+
+MAX_BUILD_TIME = CANV_CELLS_WIDTH
+
+BALLS = [(0, 0, 1, 1), 
+         (5, 5, -1, 1), 
+         (10, 10, 1, 1),
+         (15, 15, 1, -1)]  # (x, y, x_vel, y_vel), subject to change
+
+DIRECTIONS = ("north", "east", "south", "west")
 
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
-import random
 
 # These two lines make sure a faster SAT solver is used.
 from nnf import config
+
 config.sat_backend = "kissat"
 
 # Encoding that will store all of your constraints
 E = Encoding()
 
-# x width, y height
-CANV_WT = 1200
-CANV_HT = 800
-STAGE = [null,1,2,3,4]
-# defaults to stage 1
-current_stage = 1
 
-@proposition
-class Cursor():
-    def __init__(self,x,y,is_hori,):
-        self.x = x
-        self.y = y
-        self.is_hori = is_hori
+class Hashable:
+    def __hash__(self):
+        return hash(str(self))
 
-    def __str__(self) -> str:    
-        return f("Cursor at x:"+ self.x +" y:"+ self.y +"\nFacing "
-            + ("Horizontally" if self.is_hori else "Vertically"))
-    
-@proposition
-class Ball():
-    def __init__(self,x,y,pos_hori_dir,pos_vert_dir):
-        self.x = x
-        self.y = y
-        self.pos_hori_dir = pos_hori_dir
-        self.pos_vert_dir = pos_vert_dir
+    def __eq__(self, __value: object) -> bool:
+        return hash(self) == hash(__value)
+
+    def __repr__(self):
+        return str(self)
+
+
+########## PROPOSITION CLASSES ##########
+
+# H – This is true if the horizontal orientation is selected
+@proposition(E)
+class Horizontal(Hashable):
+    def __init__(self):
+        pass
 
     def __str__(self) -> str:
-        return f("Ball at x:{self.x} y:{self.y}, Going in:\n"+ 
-                ("Positive" if {self.pos_hori_dir} else "Negative") +"x direction\n"+ 
-                ("Positive" if {self.pos_vert_dir} else "Negative") +"y direction"
-                )
-    
-@proposition
-class Builder():
-    def __init__(self,x,y,is_hori):
-        self.x = x
-        self.y = y
-        self.is_hori = is_hori
+        return "The mouse has a horizontal orientation."
+
+
+# V – This is true if the vertical orientation is selected
+@proposition(E)
+class Vertical(Hashable):
+    def __init__(self):
+        pass
 
     def __str__(self) -> str:
-        return f("Builder at x:"+ self.x +" y:"+ self.y +"\nBuilding "
-            + ("Horizontally" if self.is_hori else "Vertically"))
+        return "The mouse has a vertical orientation."
+
+
+# M(x, y) – This is true if cell (x, y) is where the cursor/mouse is (i.e. the starting position of where the line is created)
+@proposition(E)
+class CursorPosition(Hashable):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return f"The cursor is at ({self.x}, {self.y})."
+
+
+# C(x, y, t) – This is true if cell (x, y) is captured (i.e. the black cells) at time t
+@proposition(E)
+class CapturedCell(Hashable):
+    def __init__(self, x, y, time):
+        self.x = x
+        self.y = y
+        self.time = time
+
+    def __str__(self) -> str:
+        return f"The cell ({self.x}, {self.y}) is captured at time {self.time}."
+
+
+# P(i, x, y, t) – This is true if ball i's position is at cell (x, y) at time t
+@proposition(E)
+class BallPosition(Hashable):
+    def __init__(self, ball_id, x, y, time):
+        self.ball_id = ball_id
+        self.x = x
+        self.y = y
+        self.time = time
+
+    def __str__(self) -> str:
+        return f"Ball {self.ball_id} is at ({self.x}, {self.y}) at time {self.time}."
+
+
+#Vx(i, t) – This is true if ball i is currently moving in the positive X direction at time t
+@proposition(E)
+class BallVelocityX(Hashable):
+    def __init__(self, ball_id, time):
+        self.ball_id = ball_id
+        self.time = time
+
+    def __str__(self) -> str:
+        return f"Ball {self.ball_id} is moving in the positive X direction at time {self.time}."
+
+
+#Vy(i, t) – This is true if ball i is currently moving in the positive Y direction at time t
+@proposition(E)
+class BallVelocityY(Hashable):
+    def __init__(self, ball_id, time):
+        self.ball_id = ball_id
+        self.time = time
+
+    def __str__(self) -> str:
+        return f"Ball {self.ball_id} is moving in the positive Y direction at time {self.time}."
+
+
+# B(D, x, y, t) – This is true if the builder of direction D (can be N, E, S, W) is at cell (x, y) at time t
+@proposition(E)
+class Builder(Hashable):
+    def __init__(self, direction, x, y, time):
+        self.direction = direction
+        self.x = x
+        self.y = y
+        self.time = time
+
+    def __str__(self) -> str:
+        return f"The {self.direction} builder is at cell ({self.x}, {self.y}) at time {self.time}"
+
+
+# L – This is true if the player will lose a life from creating a line
+@proposition(E)
+class LoseLife(Hashable):
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        return "The player will lose a life from creating a line."
+
+
+########## PROPOSITION INSTANCES ##########
+horizontal_prop = Horizontal()
+vertical_prop = Vertical()
+
+cursor_pos_props = []
+for y in range(CANV_CELLS_HEIGHT):
+    for x in range(CANV_CELLS_WIDTH):
+        cursor_pos_props.append(CursorPosition(x, y))
+
+captured_cell_props = []
+for t in range(MAX_BUILD_TIME):
+    for y in range(CANV_CELLS_HEIGHT):
+        for x in range(CANV_CELLS_WIDTH):
+            captured_cell_props.append(CapturedCell(x, y, t))
+
+ball_pos_props = []
+ball_vel_x_props = []
+ball_vel_y_props = []
+for i in range(len(BALLS)):
+    for t in range(MAX_BUILD_TIME):
+        ball_vel_x_props.append(BallVelocityX(i, t))
+        ball_vel_y_props.append(BallVelocityY(i, t))
+
+        for y in range(CANV_CELLS_HEIGHT):
+            for x in range(CANV_CELLS_WIDTH):
+                ball_pos_props.append(BallPosition(i, x, y, t))
+
+builder_props = []
+for D in DIRECTIONS:
+    for t in range(MAX_BUILD_TIME):
+        for y in range(CANV_CELLS_HEIGHT):
+            for x in range(CANV_CELLS_WIDTH):
+                builder_props.append(Builder(D, x, y, t))
+
+lose_prop = LoseLife()
+
+
+########## CONSTRAINTS ##########
+def theory():
+    # The cursor's orientation can only be either vertical or horizontal, but not both
+    E.add_constraint((Horizontal() & ~Vertical()) | (~Horizontal() & Vertical()))
+
+    # The position of a ball cannot coincide with the position of a captured cell
+    for i in range(len(BALLS)):
+        for t in range(MAX_BUILD_TIME):
+            for y in range(CANV_CELLS_HEIGHT):
+                for x in range(CANV_CELLS_WIDTH):
+                    E.add_constraint(BallPosition(i, x, y, t) >> ~CapturedCell(x, y, t))
+        
+    # There can't be both a horizontal builder and vertical builder
+    for t in range(MAX_BUILD_TIME):
+        for y in range(CANV_CELLS_HEIGHT):
+            for x in range(CANV_CELLS_WIDTH):
+                E.add_constraint((Builder("north", x, y, t) | Builder("south", x, y, t)) >> ~(Builder("east", x, y, t) | Builder("west", x, y, t)))
+                E.add_constraint((Builder("east", x, y, t) | Builder("west", x, y, t)) >> ~(Builder("north", x, y, t) | Builder("south", x, y, t)))
+
+    return E
+
+if __name__ == "__main__":
+    T = theory()
+    # Don't compile until you're finished adding all your constraints!
+    T = T.compile()
+    # After compilation (and only after), you can check some of the properties
+    # of your model:
+    print("\nSatisfiable: %s" % T.satisfiable())
+    print("# Solutions: %d" % count_solutions(T))
+    print("   Solution: %s" % T.solve())
     
-# generates a game ball based on the stage number
-game_ball=[]
-for i in range(current_stage):
-    x = random.randint(0,CANV_WT)
-    y = random.randint(0,CANV_HT)
-    pos_hori_dir = bool(random.getrandbits(1))
-    pos_vert_dir = bool(random.getrandbits(1))
-    game_ball.append(Ball(x,y,pos_hori_dir,pos_vert_dir))
-
-
-# USE ONLY AS REFERENCE
-# # To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-# @proposition(E)
-# class BasicPropositions:
-
-#     def __init__(self, data):
-#         self.data = data
-
-#     def __repr__(self):
-#         return f"A.{self.data}"
-
-
-# # Different classes for propositions are useful because this allows for more dynamic constraint creation
-# # for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# # that are instances of this class must be true by using a @constraint decorator.
-# # other options include: at most one, exactly one, at most k, and implies all.
-# # For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-# @constraint.at_least_one(E)
-# @proposition(E)
-# class FancyPropositions:
-
-#     def __init__(self, data):
-#         self.data = data
-
-#     def __repr__(self):
-#         return f"A.{self.data}"
-
-# # Call your variables whatever you want
-# a = BasicPropositions("a")
-# b = BasicPropositions("b")   
-# c = BasicPropositions("c")
-# d = BasicPropositions("d")
-# e = BasicPropositions("e")
-# # At least one of these will be true
-# x = FancyPropositions("x")
-# y = FancyPropositions("y")
-# z = FancyPropositions("z")
-
-
-# # Build an example full theory for your setting and return it.
-# #
-# #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
-# #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
-# #  what the expectations are.
-# def example_theory():
-#     # Add custom constraints by creating formulas with the variables you created. 
-#     E.add_constraint((a | b) & ~x)
-#     # Implication
-#     E.add_constraint(y >> z)
-#     # Negate a formula
-#     E.add_constraint(~(x & y))
-#     # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-#     # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-#     constraint.add_exactly_one(E, a, b, c)
-
-#     return E
-
-
-# if __name__ == "__main__":
-
-#     T = example_theory()
-#     # Don't compile until you're finished adding all your constraints!
-#     T = T.compile()
-#     # After compilation (and only after), you can check some of the properties
-#     # of your model:
-#     print("\nSatisfiable: %s" % T.satisfiable())
-#     print("# Solutions: %d" % count_solutions(T))
-#     print("   Solution: %s" % T.solve())
-
-#     print("\nVariable likelihoods:")
-#     for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-#         # Ensure that you only send these functions NNF formulas
-#         # Literals are compiled to NNF here
-#         print(" %s: %.2f" % (vn, likelihood(T, v)))
-#     print()
+    sol = T.solve()
+    print(sol)
