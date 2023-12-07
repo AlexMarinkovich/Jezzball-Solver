25e577a54538ed5de4e70368a00c41b013b9bde1
AlexMarinkovich <116607801+AlexMarinkovich@users.noreply.github.com>
2023-12-06 20:08:24 -0500

    updated

--- run.py
+++ run.py
@@ -1,14 +1,11 @@
-CANV_CELLS_WIDTH = 27
-CANV_CELLS_HEIGHT = 19
+from inputs import CURSOR_ORIENTATION, BALLS, CANVAS
+
+CANV_CELLS_WIDTH = len(CANVAS[0])
+CANV_CELLS_HEIGHT = len(CANVAS)
 
 MAX_BUILD_TIME = CANV_CELLS_WIDTH
 
-BALLS = [(0, 0, 1, 1), 
-         (5, 5, -1, 1), 
-         (10, 10, 1, 1),
-         (15, 15, 1, -1)]  # (x, y, x_vel, y_vel), subject to change
-
-DIRECTIONS = ("north", "east", "south", "west")
+DIRECTIONS = ("N", "E", "S", "W")
 
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
@@ -42,7 +39,7 @@ class Horizontal(Hashable):
         pass
 
     def __str__(self) -> str:
-        return "The mouse has a horizontal orientation."
+        return "The mouse has a horizontal orientation"
 
 
 # V – This is true if the vertical orientation is selected
@@ -52,18 +49,28 @@ class Vertical(Hashable):
         pass
 
     def __str__(self) -> str:
-        return "The mouse has a vertical orientation."
+        return "The mouse has a vertical orientation"
 
 
-# M(x, y) – This is true if cell (x, y) is where the cursor/mouse is (i.e. the starting position of where the line is created)
+# # M(x, y) – This is true if cell (x, y) is where the cursor/mouse is (i.e. the starting position of where the line is created)
+# @proposition(E)
+# class CursorPosition(Hashable):
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __str__(self) -> str:
+#         return f"The cursor is at ({self.x}, {self.y})"
+
 @proposition(E)
-class CursorPosition(Hashable):
-    def __init__(self, x, y):
+class BuildingCell(Hashable):
+    def __init__(self, x, y, time):
         self.x = x
         self.y = y
+        self.time = time
 
     def __str__(self) -> str:
-        return f"The cursor is at ({self.x}, {self.y})."
+        return f"The cell at ({self.x}, {self.y}) is currently being built at {self.time}"
 
 
 # C(x, y, t) – This is true if cell (x, y) is captured (i.e. the black cells) at time t
@@ -75,7 +82,7 @@ class CapturedCell(Hashable):
         self.time = time
 
     def __str__(self) -> str:
-        return f"The cell ({self.x}, {self.y}) is captured at time {self.time}."
+        return f"The cell ({self.x}, {self.y}) is captured at time {self.time}"
 
 
 # P(i, x, y, t) – This is true if ball i's position is at cell (x, y) at time t
@@ -88,7 +95,7 @@ class BallPosition(Hashable):
         self.time = time
 
     def __str__(self) -> str:
-        return f"Ball {self.ball_id} is at ({self.x}, {self.y}) at time {self.time}."
+        return f"Ball {self.ball_id} is at ({self.x}, {self.y}) at time {self.time}"
 
 
 #Vx(i, t) – This is true if ball i is currently moving in the positive X direction at time t
@@ -99,7 +106,7 @@ class BallVelocityX(Hashable):
         self.time = time
 
     def __str__(self) -> str:
-        return f"Ball {self.ball_id} is moving in the positive X direction at time {self.time}."
+        return f"Ball {self.ball_id} is moving in the positive X direction at time {self.time}"
 
 
 #Vy(i, t) – This is true if ball i is currently moving in the positive Y direction at time t
@@ -110,7 +117,7 @@ class BallVelocityY(Hashable):
         self.time = time
 
     def __str__(self) -> str:
-        return f"Ball {self.ball_id} is moving in the positive Y direction at time {self.time}."
+        return f"Ball {self.ball_id} is moving in the positive Y direction at time {self.time}"
 
 
 # B(D, x, y, t) – This is true if the builder of direction D (can be N, E, S, W) is at cell (x, y) at time t
@@ -133,23 +140,25 @@ class LoseLife(Hashable):
         pass
 
     def __str__(self) -> str:
-        return "The player will lose a life from creating a line."
+        return "The player will lose a life from creating a line"
 
 
 ########## PROPOSITION INSTANCES ##########
 horizontal_prop = Horizontal()
 vertical_prop = Vertical()
 
-cursor_pos_props = []
-for y in range(CANV_CELLS_HEIGHT):
-    for x in range(CANV_CELLS_WIDTH):
-        cursor_pos_props.append(CursorPosition(x, y))
+# cursor_pos_props = []
+# for y in range(CANV_CELLS_HEIGHT):
+#     for x in range(CANV_CELLS_WIDTH):
+#         cursor_pos_props.append(CursorPosition(x, y))
 
 captured_cell_props = []
+building_cell_props = []
 for t in range(MAX_BUILD_TIME):
     for y in range(CANV_CELLS_HEIGHT):
         for x in range(CANV_CELLS_WIDTH):
             captured_cell_props.append(CapturedCell(x, y, t))
+            building_cell_props.append(BuildingCell(x, y, t))
 
 ball_pos_props = []
 ball_vel_x_props = []
@@ -175,8 +184,29 @@ lose_prop = LoseLife()
 
 ########## CONSTRAINTS ##########
 def theory():
+    # initialize cursor orientation
+    if CURSOR_ORIENTATION == "H":
+        E.add_constraint(Horizontal())
+    else:
+        E.add_constraint(Vertical())
+
     # The cursor's orientation can only be either vertical or horizontal, but not both
     E.add_constraint((Horizontal() & ~Vertical()) | (~Horizontal() & Vertical()))
+
+    # A captured cell stays captured
+    for t in range(MAX_BUILD_TIME-1):
+        for y in range(CANV_CELLS_HEIGHT):
+            for x in range(CANV_CELLS_WIDTH):
+                E.add_constraint(CapturedCell(i, x, y, t) >> CapturedCell(x, y, t+1))
+
+    # A builder builds and moves to the next cell after a step in time
+    for t in range(MAX_BUILD_TIME):
+        for y in range(CANV_CELLS_HEIGHT):
+            for x in range(CANV_CELLS_WIDTH):
+                E.add_constraint(Builder("N", x, y, t) & (y>0) >> Builder("N", x, y-1, t))
+                E.add_constraint(Builder("S", x, y, t) & (y<CANV_CELLS_HEIGHT-1) >> Builder("S", x, y+1, t))
+                E.add_constraint(Builder("W", x, y, t) & (x>0) >> Builder("W", x, x-1, t))
+                E.add_constraint(Builder("E", x, y, t) & (y<CANV_CELLS_WIDTH-1) >> Builder("E", x+1, y, t))
 
     # The position of a ball cannot coincide with the position of a captured cell
     for i in range(len(BALLS)):
@@ -189,8 +219,8 @@ def theory():
     for t in range(MAX_BUILD_TIME):
         for y in range(CANV_CELLS_HEIGHT):
             for x in range(CANV_CELLS_WIDTH):
-                E.add_constraint((Builder("north", x, y, t) | Builder("south", x, y, t)) >> ~(Builder("east", x, y, t) | Builder("west", x, y, t)))
-                E.add_constraint((Builder("east", x, y, t) | Builder("west", x, y, t)) >> ~(Builder("north", x, y, t) | Builder("south", x, y, t)))
+                E.add_constraint((Builder("N", x, y, t) | Builder("S", x, y, t)) >> ~(Builder("E", x, y, t) | Builder("W", x, y, t)))
+                E.add_constraint((Builder("E", x, y, t) | Builder("W", x, y, t)) >> ~(Builder("N", x, y, t) | Builder("S", x, y, t)))
 
     return E
 
@@ -201,7 +231,7 @@ if __name__ == "__main__":
     # After compilation (and only after), you can check some of the properties
     # of your model:
     print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
+    # print("# Solutions: %d" % count_solutions(T))
     print("   Solution: %s" % T.solve())
     
     sol = T.solve()
